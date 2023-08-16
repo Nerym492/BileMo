56ce58c0faa1fb1e3199a08632a18d39d2a42924
Florian Pohu <florianpohu49@gmail.com>
2023-08-16 10:19:02 +0200

    Add groups getUsers

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
@@ -12,15 +13,19 @@ class User
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['getUsers'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 50)]
+    #[Groups(['getUsers'])]
     private ?string $firstName = null;
 
     #[ORM\Column(length: 50)]
+    #[Groups(['getUsers'])]
     private ?string $lastName = null;
 
     #[ORM\Column(length: 180)]
+    #[Groups(['getUsers'])]
     private ?string $email = null;
 
     #[ORM\ManyToOne(inversedBy: 'users')]
