3d3b4996a9a0a716f43edf06fd181a1f24dc4d52
Florian Pohu <florianpohu49@gmail.com>
2023-08-16 10:20:55 +0200

    Add query to get users by customer

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -21,20 +21,20 @@ class UserRepository extends ServiceEntityRepository
         parent::__construct($registry, User::class);
     }
 
-    //    /**
-    //     * @return User[] Returns an array of User objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('u')
-    //            ->andWhere('u.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('u.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
+    /**
+     * @return User[] Returns an array of User objects
+     */
+    public function findByCustomer(string $email): array
+    {
+        return $this->createQueryBuilder('u')
+            ->leftJoin('u.customer', 'c')
+            ->andWhere('c.email = :customerEmail')
+            ->setParameter('customerEmail', $email)
+            ->orderBy('u.id', 'ASC')
+            ->getQuery()
+            ->getResult()
+        ;
+    }
 
     //    public function findOneBySomeField($value): ?User
     //    {
