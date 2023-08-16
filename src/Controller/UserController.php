7b51df0963d1e840b6c3d53d83ae02e40b3eaded
Florian Pohu <florianpohu49@gmail.com>
2023-08-16 11:15:57 +0200

    Add createUser route

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,10 +2,10 @@
 
 namespace App\Controller;
 
-use App\Entity\Customer;
 use App\Entity\User;
 use App\Repository\CustomerRepository;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use JMS\Serializer\SerializationContext;
 use JMS\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -15,12 +15,14 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class UserController extends AbstractController
 {
     public function __construct(
         private UserRepository $userRepository,
         private CustomerRepository $customerRepository,
+        private EntityManagerInterface $entityManager,
         private Security $security,
         private SerializerInterface $serializer
     ) {
@@ -36,5 +38,30 @@ class UserController extends AbstractController
         $jsonUsers = $this->serializer->serialize($users, 'json', $context);
 
         return new JsonResponse($jsonUsers, Response::HTTP_OK, [], true);
+    }
+
+    #[Route('api/users', name: 'createUser', methods: ['POST'])]
+    #[isGranted('ROLE_CUSTOMER', message: 'You must be logged in to create a new user.')]
+    public function addUser(Request $request, ValidatorInterface $validator): JsonResponse
+    {
+        $user = $this->serializer->deserialize($request->getContent(), User::class, 'json');
+        $errors = $validator->validate($user);
+
+        if ($errors->count() > 0) {
+            return new JsonResponse($this->serializer->serialize($errors, 'json'));
+        }
+
+        // Link the logged customer to the created user
+        $loggedCustomerMail = $this->security->getUser()->getUserIdentifier();
+        $loggedCustomer = $this->customerRepository->findOneBy(['email' => $loggedCustomerMail]);
+        $user->setCustomer($loggedCustomer);
+
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+
+        $context = SerializationContext::create()->setGroups('getUsers');
+        $jsonUser = $this->serializer->serialize($user, 'json', $context);
+
+        return new JsonResponse($jsonUser, Response::HTTP_CREATED, [], true);
     }
 }
