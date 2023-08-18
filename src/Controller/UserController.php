fddd19dacde75c2fe00e18c63e5590e4f4999f13
Florian Pohu <florianpohu49@gmail.com>
2023-08-18 09:23:47 +0200

    Add detailUser and deleteUser routes

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -29,7 +29,7 @@ class UserController extends AbstractController
     }
 
     #[Route('/api/users', name: 'users', methods: ['GET'])]
-    #[IsGranted('ROLE_CUSTOMER', message: 'You must be logged in to view the list of users.')]
+    #[IsGranted('ROLE_CUSTOMER', message: 'You do not have the required rights to view the list of users.')]
     public function getUserList(): JsonResponse
     {
         $users = $this->userRepository->findByCustomer($this->security->getUser()->getUserIdentifier());
@@ -41,7 +41,7 @@ class UserController extends AbstractController
     }
 
     #[Route('api/users', name: 'createUser', methods: ['POST'])]
-    #[isGranted('ROLE_CUSTOMER', message: 'You must be logged in to create a new user.')]
+    #[isGranted('ROLE_CUSTOMER', message: 'You do not have the required rights to create a new user.')]
     public function addUser(Request $request, ValidatorInterface $validator): JsonResponse
     {
         $user = $this->serializer->deserialize($request->getContent(), User::class, 'json');
@@ -68,5 +68,45 @@ class UserController extends AbstractController
         $jsonUser = $this->serializer->serialize($user, 'json', $context);
 
         return new JsonResponse($jsonUser, Response::HTTP_CREATED, [], true);
+    }
+
+    #[Route('/api/users/{id}', name: 'detailUser', methods: ['GET'])]
+    #[IsGranted('ROLE_CUSTOMER', message: 'You do not have the required rights to see a detailed user.')]
+    public function getDetailUser(User $user): JsonResponse
+    {
+        $context = SerializationContext::create()->setGroups('getUsers');
+        $users = $this->userRepository->findByCustomer(
+            $this->security->getUser()->getUserIdentifier(),
+            $user
+        );
+
+        $jsonUsers = $this->serializer->serialize($users, 'json', $context);
+
+        return new JsonResponse($jsonUsers, Response::HTTP_OK, [], true);
+    }
+
+    #[Route('/api/users/{id}', name: 'deleteUser', methods: ['DELETE'])]
+    #[IsGranted('ROLE_CUSTOMER', message: 'You do not have the required rights to delete a user.')]
+    public function deleteUser(User $user): JsonResponse
+    {
+        $customerMail = $this->security->getUser()->getUserIdentifier();
+        $customerId = $this->customerRepository->findOneBy(['email' => $customerMail]);
+
+        if ($user->getCustomer()->getId() !== $customerId) {
+            $errorMessage = ['error' => 'Your are not authorized to delete this user.'];
+            $jsonErrorMessage = $this->serializer->serialize($errorMessage, 'json');
+
+            return new JsonResponse(
+                $jsonErrorMessage,
+                Response::HTTP_BAD_REQUEST,
+                [],
+                true
+            );
+        }
+
+        $this->entityManager->remove($user);
+        $this->entityManager->flush();
+
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
     }
 }
