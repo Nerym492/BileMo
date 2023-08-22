88154b1a985a59f6da994f834b1c7b6873e11270
Florian Pohu <florianpohu49@gmail.com>
2023-08-22 14:49:24 +0200

    Add product self-discoverability relation

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -6,7 +6,18 @@ use App\Repository\ProductRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use JMS\Serializer\Annotation\Groups;
+use Hateoas\Configuration\Annotation as Hateoas;
 
+/**
+ * @Hateoas\Relation(
+ *      "self",
+ *      href = @Hateoas\Route(
+ *          "detailProduct",
+ *          parameters = { "id" = "expr(object.getId())" }
+ *      ),
+ *      exclusion = @Hateoas\Exclusion(groups={"getProducts"})
+ * )
+ */
 #[ORM\Entity(repositoryClass: ProductRepository::class)]
 class Product
 {
